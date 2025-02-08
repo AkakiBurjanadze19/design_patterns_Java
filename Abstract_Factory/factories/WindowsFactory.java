f19c16ff761856b89691cacd74e95a554cb609fe
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-09 01:22:40 +0400

    add comments

--- Abstract_Factory/factories/WindowsFactory.java
+++ Abstract_Factory/factories/WindowsFactory.java
@@ -5,6 +5,7 @@ import Abstract_Factory.checkboxes.Checkbox;
 import Abstract_Factory.buttons.WindowsButton;
 import Abstract_Factory.checkboxes.WindowsCheckbox;
 
+/* Each concrete factory extends basic factory and is responsible for creating products of single variety. */
 public class WindowsFactory implements GUIFactory {
     @Override
     public Button createButton() {
