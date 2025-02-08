520ce08c5e1f995d34a317638539a2267133ec0e
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-09 01:21:20 +0400

    add comments

--- Abstract_Factory/buttons/Button.java
+++ Abstract_Factory/buttons/Button.java
@@ -1,5 +1,7 @@
 package Abstract_Factory.buttons;
 
+/* Abstract factory assumes that you have several families of products. */
+/* All products of the same family have the common interface. */
 public interface Button {
     void paint();
 }
