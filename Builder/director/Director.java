745125b6a22f270dc8235642a7b54c33569b9ba8
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-09 01:10:30 +0400

    add comments

--- Builder/director/Director.java
+++ Builder/director/Director.java
@@ -7,6 +7,10 @@ import Builder.components.GPSNavigator;
 import Builder.components.Transmission;
 import Builder.components.TripComputer;
 
+/*
+    * Director defines the order of building steps. It works with a Builder object through the common builder
+    * interface. Therefore, it may not know what product is being built.
+*/
 public class Director {
     public void constructSportsCar(Builder builder) {
         builder.setCarType(CarType.SPORTS_CAR);
