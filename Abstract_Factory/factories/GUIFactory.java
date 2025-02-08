3338fa6813c76f5547d0032180c8eb5ba084675b
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-09 01:22:23 +0400

    add comments

--- Abstract_Factory/factories/GUIFactory.java
+++ Abstract_Factory/factories/GUIFactory.java
@@ -3,6 +3,7 @@ package Abstract_Factory.factories;
 import Abstract_Factory.buttons.Button;
 import Abstract_Factory.checkboxes.Checkbox;
 
+/* Abstract factory knows about all (abstract) products */
 public interface GUIFactory {
     Button createButton();
     Checkbox createCheckbox();
