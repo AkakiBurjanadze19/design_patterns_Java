17eb3dda0ae8aa7bbe51a84ba53f423534c8f36b
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-09 01:22:51 +0400

    add comments

--- Abstract_Factory/app/Application.java
+++ Abstract_Factory/app/Application.java
@@ -4,6 +4,8 @@ import Abstract_Factory.buttons.Button;
 import Abstract_Factory.checkboxes.Checkbox;
 import Abstract_Factory.factories.GUIFactory;
 
+/* Factory users don't care which concrete factory they use since they work with factories and products through
+abstract interfaces. */
 public class Application {
     private Button button;
     private Checkbox checkbox;
