76aae1f265afb6f24f104f8c92d267b9f496fcf1
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-09 18:55:33 +0400

    add Demo.java

---
+++ Singleton/multithreaded/Demo.java
@@ -0,0 +1,27 @@
+package Singleton.multithreaded;
+
+public class Demo {
+    public static void main(String[] args) {
+        Thread threadFoo = new Thread(new ThreadFoo());
+        Thread threadBar = new Thread(new ThreadBar());
+
+        threadFoo.start();
+        threadBar.start();
+    }
+
+    static class ThreadFoo implements Runnable {
+        @Override
+        public void run() {
+            Singleton singleton = Singleton.getInstance("FOO");
+            System.out.println(singleton.value);
+        }
+    }
+
+    static class ThreadBar implements Runnable {
+        @Override
+        public void run() {
+            Singleton anotherSingleton = Singleton.getInstance("BAR");
+            System.out.println(anotherSingleton.value);
+        }
+    }
+}
