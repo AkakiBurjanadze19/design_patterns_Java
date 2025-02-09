eeba3abb803e296e7b8465c809c5fe9c27d9e608
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-09 20:39:05 +0400

    add TV.java

---
+++ Bridge/devices/TV.java
@@ -0,0 +1,53 @@
+package Bridge.devices;
+
+public class TV implements Device {
+    private boolean on = false;
+    private int volume = 30;
+    private int channel = 1;
+
+    @Override
+    public boolean isEnabled() {
+        return this.on;
+    }
+
+    @Override
+    public void enable() {
+        this.on = true;
+    }
+
+    @Override
+    public void disable() {
+        this.on = false;
+    }
+
+    @Override
+    public int getVolume() {
+        return this.volume;
+    }
+
+    @Override
+    public void setVolume(int percent) {
+        this.volume = percent;
+    }
+
+    @Override
+    public int getChannel() {
+        return this.channel;
+    }
+
+    @Override
+    public void setChannel(int channel) {
+        this.channel = channel;
+    }
+
+    @Override
+    public void printStatus() {
+        System.out.println("---------");
+        System.out.println("Welcome to TV!");
+        System.out.println("I am " + (on ? "enabled" : "disabled"));
+        System.out.println("Current volume is " + this.volume + "%");
+        System.out.println("Current channel is: " + this.channel);
+        System.out.println("---------");
+        System.out.println();
+    }
+}
