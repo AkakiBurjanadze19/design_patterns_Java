a6d5cca828b61a32563aa96ce605adee65c03ec4
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-09 19:43:40 +0400

    add RoundHole.java

---
+++ Adapter/round/RoundHole.java
@@ -0,0 +1,18 @@
+package Adapter.round;
+
+/* Round holes are compatible with round pegs */
+public class RoundHole {
+    private double radius;
+
+    public RoundHole(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public boolean fits(RoundPeg peg) {
+        return this.getRadius() >= peg.getRadius();
+    }
+}
