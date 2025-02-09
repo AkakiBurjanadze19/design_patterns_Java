951d0ea6ad92b99ee248aaa5c0f76a6e4354bb70
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-09 20:39:36 +0400

    add Remote.java

---
+++ Bridge/remotes/Remote.java
@@ -0,0 +1,10 @@
+package Bridge.remotes;
+
+/* Common interface for all remotes */
+public interface Remote {
+    void power();
+    void volumeDown();
+    void volumeUp();
+    void channelDown();
+    void channelUp();
+}
