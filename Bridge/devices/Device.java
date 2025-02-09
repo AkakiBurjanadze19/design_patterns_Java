4ac289fe75554721a80a2c27c6aff7a76bdce579
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-09 20:38:52 +0400

    add Device.java

---
+++ Bridge/devices/Device.java
@@ -0,0 +1,13 @@
+package Bridge.devices;
+
+/* Common interface for all devices */
+public interface Device {
+    boolean isEnabled();
+    void enable();
+    void disable();
+    int getVolume();
+    void setVolume(int percent);
+    int getChannel();
+    void setChannel(int channel);
+    void printStatus();
+}
