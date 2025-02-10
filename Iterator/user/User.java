afe7952cc85ff23f66310436c6b2312d1d2bff83
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-10 22:06:20 +0400

    add User.java

---
+++ Iterator/user/User.java
@@ -0,0 +1,25 @@
+package Iterator.user;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public User(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+}
