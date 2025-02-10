ea1eaab832cbc9f7fdcfde1994997003f2750471
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-11 00:16:36 +0400

    add ArithmeticOperationsStrategy.java

---
+++ Strategy/example2/strategies/ArithmeticOperationsStrategy.java
@@ -0,0 +1,6 @@
+package Strategy.example2.strategies;
+
+/* Common interface for all arithmetic operations */
+public interface ArithmeticOperationsStrategy {
+    void performOperation(int a, int b);
+}
